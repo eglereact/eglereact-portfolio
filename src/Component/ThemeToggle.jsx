27e8d296a47ffc5400138f4c3035fc51fd98a643
eglereact <eglereactjs@gmail.com>
2024-08-14 18:07:28 +0300

    dark mode basics

---
+++ src/Component/ThemeToggle.jsx
@@ -0,0 +1,18 @@
+import { FaSun, FaMoon } from "react-icons/fa";
+import { useGlobalContext } from "../Context/context";
+
+const ThemeToggle = () => {
+  const { isDarkTheme, toggleDarkTheme } = useGlobalContext();
+  return (
+    <section className="toggle-container">
+      <button className="dark-toggle" onClick={toggleDarkTheme}>
+        {isDarkTheme ? (
+          <FaMoon className="toggle-icon" />
+        ) : (
+          <FaSun className="toggle-icon" />
+        )}
+      </button>
+    </section>
+  );
+};
+export default ThemeToggle;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+import ThemeToggle from "../Component/ThemeToggle";
 
 const HomePage = () => {
   return (
     <div>
       <h1>Home Page</h1>
+      <ThemeToggle />
       <Link to="/projects">Projects</Link>
       <Link to="/contacts">Contacts</Link>
       <Link to="/about-me">About Me</Link>
