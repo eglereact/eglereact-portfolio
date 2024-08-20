787a1949f4ad1c0cf965ebe10886602f13333a49
eglereact <eglereactjs@gmail.com>
2024-08-20 17:07:36 +0300

    add a video to project page

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -1,8 +1,47 @@
 import { useParams } from "react-router-dom";
+import { projects } from "../Data/projectsData";
+import Navigation from "../Component/Navigation";
 
 const ProjectPage = () => {
   const params = useParams();
 
-  return <div>ProjectPage {params.projectId}</div>;
+  const videoUrl =
+    "https://drive.google.com/file/d/1afvlQvcGNVil1mODBbTlhiTpd2o-G82W/preview";
+
+  const project = params.projectId
+    ? projects.find((p) => p.id === +params.projectId)
+    : projects[0];
+
+  if (!project) {
+    return <div>Project not found</div>;
+  }
+
+  return (
+    <>
+      <Navigation />
+      <section className="project-page-container">
+        <div className="project-page-info">
+          <div className="project-page-maintext">
+            <h1>{project.name}</h1>
+            <h1>{project.id}</h1>
+            <p>{project.info}</p>
+          </div>
+          <div className="project-page-otherinfo"></div>
+        </div>
+        <div>
+          <div>
+            <iframe
+              src={videoUrl}
+              width="1096"
+              height="685"
+              allow="autoplay"
+              title="Google Drive Video"
+              allowFullScreen
+            ></iframe>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 export default ProjectPage;
