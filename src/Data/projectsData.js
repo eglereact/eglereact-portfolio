45f020d690d37efff3f134028e9c88c51d17f0e0
eglereact <eglereactjs@gmail.com>
2024-10-01 12:30:27 +0300

    and more info

--- src/Data/projectsData.js
+++ src/Data/projectsData.js
@@ -22,17 +22,20 @@ export const projects = [
   {
     id: 2,
     name: "React fund me",
-    info: "Mini crud bank operation",
-    img: "/eglereact-portfolio/images/loptop.png",
+    info: "Finansavimo platforma leidžianti vartotojams kurti savo kampanijas ir aukoti kitoms.",
+    img: "/eglereact-portfolio/images/fund.jpg",
     videoUrl:
-      "https://drive.google.com/file/d/1afvlQvcGNVil1mODBbTlhiTpd2o-G82W/preview",
+      "https://drive.google.com/file/d/1XSWjdz3ewHddx4zdmmXoTfTvu0Uzd6HH/preview",
     used: [
-      "React biblioteką",
-      "Express serverį",
-      "MySql duomenų bazę",
-      "TailwindCSS",
+      { icon: <FaReact />, text: "React biblioteką" },
+      { icon: <FaNode />, text: "Express serverį" },
+      { icon: <SiMysql />, text: "MySQL duomenų bazę" },
+      { icon: <SiTailwindcss />, text: "TailwindCSS" },
     ],
+    description:
+      "React Fund Me yra finansavimo platforma, leidžianti vartotojams lengvai kurti ir valdyti lėšų rinkimo kampanijas. Vartotojai gali lengvai prisijungti ar užsiregistruoti, kad sukurtų kampanijas, o administratoriaus skydelyje suteikiama visiška kontrolė valdant įrašus, aukas ir vartotojų paskyras. Valdymo panelė supaprastina turinio pridėjimo, redagavimo ir šalinimo procesą, užtikrindama sklandų visų lėšų rinkimo aspektų valdymą.",
   },
+
   {
     id: 3,
     name: "Something else",
